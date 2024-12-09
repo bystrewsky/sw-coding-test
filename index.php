92567637b21e6cff400bc4afe15a23af99350e06
bystrewsky <igor.bystrewsky@gmail.com>
2024-12-08 19:59:26 -0500

    Refactor application entry point

--- index.php
+++ index.php
@@ -1,28 +1,8 @@
 <?php
-require_once './src/models/Comment.php';
-require_once './src/controllers/CommentController.php';
+require './src/core/App.php';
 
-$db_config = require('./config/db.php');
+$app = new App();
 
-try {
-    $db = new PDO( 
-        "mysql:host={$db_config['host']};dbname={$db_config['dbname']}",
-        $db_config['username'],
-        $db_config['password'],
-        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-    );
-} catch (PDOException $e) {
-    die('Failed to connect to the database: ' . $e->getMessage());
-}
-
-$commentModel = new Comment($db);
-$commentController = new CommentController($commentModel);
-
-// Uncomment one of the following lines to populate/reset Ship Date
-
-// $commentController->resetShipDate();
-// $commentController->populateShipDate();
-
-$commentController->index();
+$app->run();
 
 ?>
---
+++ src/core/App.php
@@ -0,0 +1,39 @@
+<?php
+require __DIR__ . '/../models/Comment.php';
+require __DIR__ . '/../controllers/CommentController.php';
+class App {
+    private $db;
+
+    public function __construct() {
+        $this->connectDatabase();
+    }
+
+    private function connectDatabase() {
+        $db_config = require __DIR__ . '/../../config/db.php';
+
+        try {
+            $this->db = new PDO( 
+                "mysql:host={$db_config['host']};dbname={$db_config['dbname']}",
+                $db_config['username'],
+                $db_config['password'],
+                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+            );
+        } catch (PDOException $e) {
+            die('Failed to connect to the database: ' . $e->getMessage());
+        }
+    }
+
+    public function run() {
+        $commentModel = new Comment($this->db);
+        $commentController = new CommentController($commentModel);
+        
+        // Uncomment one of the following lines to populate/reset Ship Date
+        
+        // $commentController->resetShipDate();
+        // $commentController->populateShipDate();
+        
+        $commentController->index();
+    }
+}
+
+?>
